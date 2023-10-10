51baf6a0614f1a158d77c53f6537ffadd8bdae09
JSusijs <gugulisrugulis@gmail.com>
2023-10-10 15:16:32 +0300

    commit 1

--- testconsole1.py
+++ testconsole1.py
@@ -1,13 +1,19 @@
-from tkinter import *
-from tkinter import ttk
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
-root = Tk()
-root.title("BBot")
-root.geometry("400x400")
-frm = ttk.Frame(root, padding=100)
-frm.grid()
-ttk.Label(frm, text="text").grid(column=0, row=0)
+driver = webdriver.Firefox()
+driver.get("http://help.websiteos.com/websiteos/example_of_a_simple_html_page.htm")
 
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=1)
+elem = driver.find_element(By.TAG_NAME, "h1")
+print(elem.text)
 
-root.mainloop()
+elements = driver.find_elements(By.CLASS_NAME, "whs2")
+
+for i in range(0, len(elements)):
+    print(elements[i].text)
+
+print(elements[3].text)
+
+assert "No results found." not in driver.page_source
+driver.close()
