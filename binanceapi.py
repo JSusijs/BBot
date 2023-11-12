90c8c8cdf07d5c995df06252fc3fa09c773f71f7
JSusijs <gugulisrugulis@gmail.com>
2023-11-12 23:22:55 +0200

    More coefficients and similar

---
+++ binanceapi.py
@@ -0,0 +1,48 @@
+import time
+import logging
+from binance.lib.utils import config_logging
+from binance.um_futures import UMFutures
+from binance.websocket.um_futures.websocket_client import UMFuturesWebsocketClient
+from binance.error import ClientError
+
+config_logging(logging, logging.DEBUG)
+
+info = ["None"]
+
+
+def message_handler(_, message):
+    info[0] = message
+
+
+um_futures_client = UMFutures(key='', secret='')
+my_client = UMFuturesWebsocketClient(on_message=message_handler, is_combined=True)
+my_client.agg_trade(symbol="GALAUSDT", id=1)
+#logging.info(um_futures_client.account(recvWindow=6000))
+
+time.sleep(1)
+
+for i in range(0, 15):
+    time.sleep(1)
+    print(info[0])
+
+time.sleep(1)
+
+my_client.agg_trade(symbol="GALAUSDT", id=1, action=UMFuturesWebsocketClient.ACTION_UNSUBSCRIBE)
+time.sleep(1)
+
+listen_key = um_futures_client.new_listen_key()['listenKey']
+print(listen_key)
+
+try:
+    response = um_futures_client.get_all_orders(symbol="GALAUSDT", recvWindow=2000)
+    logging.info(response)
+except ClientError as error:
+    logging.error(
+        "Found error. status: {}, error code: {}, error message: {}".format(
+            error.status_code, error.error_code, error.error_message
+        )
+    )
+
+time.sleep(1)
+logging.info(um_futures_client.close_listen_key(listen_key))
+my_client.stop()
