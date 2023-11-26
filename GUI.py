9c5be7fc42bb4b75c1aae62c06453700d1e0cd82
CrackRocketeer <118108586+CrackRocketeer@users.noreply.github.com>
2023-11-26 21:38:40 +0200

    Update GUI.py
    Gatavs GUI

--- GUI.py
+++ GUI.py
@@ -6,13 +6,14 @@ button_size = (7, 1)
 # Create the main window
 layout = [
     [
-        sg.Button("START", size=button_size, font=("Helvetica", 30, "bold"), key='-START-', button_color=("black", "green")),
-        sg.Button("EXIT", size=button_size, font=("Helvetica", 30, "bold"), key='-EXIT-', button_color=("black", "red"))
+        sg.Button("START", size=button_size, font=("Helvetica", 30, "bold"),  pad=(4, 5), key='-START-', button_color=("black", "green")),
+        sg.Button("EXIT", size=button_size, font=("Helvetica", 30, "bold"), pad=(4, 5), key='-EXIT-', button_color=("black", "red"))
     ],
     [
-        sg.Button("INSTRUCTION", size=(15, 1), font=("Helvetica", 30, "bold"), key='-INSTRUCTION-', button_color=("black", "orange"))
+        [sg.Button("INSTRUCTION", size=(15, 1), font=("Helvetica", 30, "bold"), key='-INSTRUCTION-', button_color=("black", "orange"), pad=(5, 3))],
     ]
 ]
+
 
 window = sg.Window("Binance Trading Bots", layout, background_color="#4a4a4a", finalize=True)
 active_window = 'main'  # Track the active window
@@ -26,9 +27,9 @@ def create_start_window():
              sg.Text((""), pad=(16, 0), background_color="#4a4a4a"),
              sg.Text(("Select grid type:"), background_color="#4a4a4a", font=("Helvetica", 25))],
 
-            [sg.Combo(['≤1 Day', '1-2 Days', '2-7 Days', '7-15 Days', '15-30 Days', ' ≥30 Days'], key='combo', size=(14, 1), font=("Helvetica", 20)),
+            [sg.Combo(['≤1 Day', '1-2 Days', '2-7 Days', '7-15 Days', '15-30 Days', ' ≥30 Days'], key='-RUNTIME-', size=(14, 1), font=("Helvetica", 20)),
              sg.Text((""), pad=(10, 0), background_color="#4a4a4a"),
-             sg.Combo(['Futures grid', 'Spot grid'], key='combo', size=(14, 1), font=("Helvetica", 20))],
+             sg.Combo(['Futures grid', 'Spot grid'], key='-GRIDTYPE-', size=(14, 1), font=("Helvetica", 20))],
 
             [sg.Button('START ANALYSIS', size=(33, 1), font=("Helvetica", 20), key='-SUBMIT-', button_color=("black", "green"))],
             [sg.Button("BACK TO MAIN MENU", size=(33, 1), font=("Helvetica", 20), key='-BACK-', button_color=("black", "orange"))]
@@ -50,8 +51,40 @@ def create_start_window():
         if start_event == sg.WIN_CLOSED:  # Check for the X button
             break
 
+        if start_event == '-SUBMIT-':
+            selected_runtime = start_values['-RUNTIME-']
+            selected_gridtype = start_values['-GRIDTYPE-']
 
+            table_data = [
+                ['Parameter 1', 'Value 1', 'Parameter 1', 'Value 1', 'Parameter 1'],
+                ['Parameter 2', 'Value 2'],
+                ['Parameter 3', 'Value 3'],
+                ['Parameter 4', 'Value 4'],
+                ['Parameter 5', 'Value 5'],
+            ]
 
+            table_headings = ['Symbol', 'strategyID', 'Runtime', 'ROI', 'Coef']
+
+            start_window.close()
+            hello_layout = [
+                [sg.Text(f"Selected Runtime: {selected_runtime}", background_color="#4a4a4a", font=("Helvetica", 26, "bold"))],
+                [sg.Text(f"Selected Grid Type: {selected_gridtype}", background_color="#4a4a4a", font=("Helvetica", 26, "bold"))],
+                [sg.Table(values=table_data, headings=table_headings, size=(150, 40), background_color="#4a4a4a", font=("Helvetica", 15, "bold"), auto_size_columns=False, justification='center', key='-TABLE-')],
+                [sg.Button("BACK TO MAIN MENU", size=(46, 1), font=("Helvetica", 20), key='-OK-', button_color=("black", "orange"))]
+
+            ]
+            hello_window = sg.Window("Selected Parameters", hello_layout, finalize=True, background_color="#4a4a4a")
+
+            while True:
+                hello_event, hello_values = hello_window.read()
+
+                if hello_event == sg.WIN_CLOSED or hello_event == '-OK-':
+                    hello_window.close()
+                    break
+
+    start_window.close()
+
+# Create a function to create and show the instruction window
 def create_instruction_window():
     instruction_layout = [
         [sg.Text("Welcome to user manual for Binance Trading Bots application", background_color="#4a4a4a", font=("Helvetica", 30, "bold"))],
@@ -86,6 +119,7 @@ def create_instruction_window():
             instruction_window.close()
             return_to_main_window()
 
+# Create a function to return to the main window
 def return_to_main_window():
     global active_window
     active_window = 'main'
